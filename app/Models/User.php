648ad3c047fe76b995b9db89e8a6c192ce3a8956
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2024-01-18 14:18:14 +0630

    install spatie permission and create fake data

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasPermissions;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
+    use HasPermissions;
+    use HasRoles;
 
     /**
      * The attributes that should be hidden for serialization.
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -13,11 +14,28 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // Super Admin
+        $superAdmin = User::factory()->create([
+           'name' => 'Super Admin',
+           'role' => 'admin',
+           'email' => 'superadmin@gmail.com',
+           'password' => 'Password!',
+           'status' => 'active'
+        ]);
+
+        $superAdmin->assignRole(1);
+
+        $role = Role::with('permissions')->find(1);
+
+        $superAdmin->syncPermissions($role->permissions);
+
+        // Admin
         User::factory()->create([
             'name' => 'Admin',
             'role' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => 'Password!',
+            'status' => 'active'
         ]);
     }
 }
